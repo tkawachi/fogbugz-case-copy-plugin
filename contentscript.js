213c3fd3d8fa0edbacaf0f8a4f6b40233692fa6d
KAWACHI Takashi <kawachi@p-lucky.net>
2013-05-31 00:19:20 +0900

    Copy with Cmd-C or Ctrl-C

--- contentscript.js
+++ contentscript.js
@@ -1,22 +1,41 @@
 var casePageUrlRe = /^(.+\.fogbugz\.com\/default\.asp\?\d+)/;
+var isMac = navigator.userAgent.indexOf('Mac') >= 0;
+var isWin = navigator.userAgent.indexOf('Win') >= 0;
+
+function copyFromPage(sendResponse) {
+    var m = casePageUrlRe.exec(document.location.href);
+    if (m) {
+	chrome.extension.sendMessage({
+	    command: "copyLink",
+	    link: {
+		url: m[1],
+		title: $("title").text()
+	    }
+	}, function(resp) {
+	    sendResponse({message: "Copied!"});
+	});
+    } else {
+	sendResponse({message: "None copied"});
+    }
+    return true;
+}
 
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    console.log(msg);
+//    console.log(msg);
     if (msg.command == "copyFromPage") {
-	var m = casePageUrlRe.exec(document.location.href);
-	if (m) {
-	    chrome.extension.sendMessage({
-		command: "copyLink",
-		link: {
-		    url: m[1],
-		    title: $("title").text()
-		}
-	    }, function(resp) {
-		sendResponse({message: "Copied!"});
-	    });
-	} else {
-	    sendResponse({message: "None copied"});
-	}
-	return true;
+	return copyFromPage(sendResponse);
     }
+});
+
+$(function() {
+    document.onkeydown = function (e) {
+	var selType = window.getSelection().type;
+	if (e.keyCode == "C".charCodeAt(0) &&
+	    ((isMac && e.metaKey) || (isWin && e.ctrlKey)) &&
+	    (selType == "None" || selType == "Caret")
+	   ) {
+	    // Cmd-c or Ctrl-c pressed without selection
+	    copyFromPage(function(){});
+	}
+    };
 });
