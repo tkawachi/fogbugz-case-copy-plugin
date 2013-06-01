e058ee4a6ba67b7ff4fe966e0c4e3776024b49fa
KAWACHI Takashi <kawachi@p-lucky.net>
2013-06-01 12:42:49 +0900

    Match with empty path

--- contentscript.js
+++ contentscript.js
@@ -4,25 +4,25 @@ var isMac = navigator.userAgent.indexOf('Mac') >= 0;
 var isWin = navigator.userAgent.indexOf('Win') >= 0;
 
 function copyFromPage(sendResponse) {
-    var m1 = casePageUrlRe.exec(document.location.href);
-    var m2 = listPageUrlRe.exec(document.location.href);
-    if (m1) {
+    if (casePageUrlRe.exec(document.location.href)) {
 	var title = $("title").text().replace(" - FogBugz", "");
 	chrome.extension.sendMessage({
 	    command: "copyLink",
 	    link: {
-		url: m1[1],
+		url: RegExp.$1,
 		title: title
 	    }
 	}, function(resp) {
 	    sendResponse({message: "Copied!"});
 	});
-    } else if (m2 && $(listPageSel).size() > 0) {
+    } else if (listPageUrlRe.exec(document.location.href) &&
+	       $(listPageSel).size() > 0) {
+	var urlBase = RegExp.$1;
 	var links = $(listPageSel).map(function () {
 	    var bugId = $(this).val();
 	    var title = $(this).closest("tr").find(".title.uvb,.title.vb").text();
 	    return {
-		url: m2[1] + bugId,
+		url: urlBase + "/default.asp?" +  bugId,
 		title: bugId + ": " + title
 	    };
 	}).get();
@@ -50,10 +50,11 @@ chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
 $(function() {
     document.onkeydown = function (e) {
 	var selType = window.getSelection().type;
-	if (e.keyCode == "C".charCodeAt(0) &&
-	    ((isMac && e.metaKey) || (isWin && e.ctrlKey)) &&
-	    (selType == "None" || selType == "Caret")
-	   ) {
+	if (
+	    e.keyCode == "C".charCodeAt(0) &&
+		((isMac && e.metaKey) || (!isMac && e.ctrlKey)) &&
+		(selType == "None" || selType == "Caret")
+	) {
 	    // Cmd-c or Ctrl-c pressed without selection
 	    copyFromPage(function(){});
 	}
