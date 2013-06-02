c135059385b3bfb66a64e2f1898ed02c0e6ea9a3
KAWACHI Takashi <kawachi@p-lucky.net>
2013-06-02 18:42:51 +0900

    Updated listPageUrlRe

--- src/shared.js
+++ src/shared.js
@@ -1,3 +1,6 @@
 // shared script between content script and extension code.
+
+// Url for each case page
 var casePageUrlRe = /^(.+\.fogbugz\.com\/default\.asp\?\d+)/;
-var listPageUrlRe = /^(.+\.fogbugz\.com\/)(?:default\.asp\?.*pg=pgList.*)?$/;
+// Url for list page
+var listPageUrlRe = /^(.+\.fogbugz\.com\/)(?:default\.asp(?:\?.*pg=pgList.*)?)?$/;
